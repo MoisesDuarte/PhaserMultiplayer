5b7c53de29b1edf341174c44d95d03a9e2f99322
MoisesDuarte <moisesduartejunior@gmail.com>
2019-10-29 13:14:53 -0300

    Implementando criação e definição de player em server-side

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -18,18 +18,51 @@ const config = {
     autoFocus: false
 };
 
-const game = new Phaser.Game(config);
+const players = {};
 
 function preload() {
-
+    this.load.image('player', 'assets/playerBlue.png');
 }
 
 function create() {
+    const self = this; // Variavel self usadap para guardar uma referencia ao Phaser Scene
+
+    this.players = this.physics.add.group();
+
     // 'Ouvindo' chamadas de conexões no servidor
     io.on('connection', function (socket) {
         console.log('Jogador conectou');
+
+        // Cria um novo jogador e adiciona ao grupo de jogadores
+        players[socket.id] = {
+            rotation: 0,
+            x: Math.floor(Math.random() * 700) + 50,
+            y: Math.floor(Math.random() * 500) + 50,
+            playerId: socket.id, // Para referenciar no jogo, caso necessario
+            team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue' // Define o time do jogador
+        };
+
+        // Adiciona jogador ao servidor
+        addPlayer(self, players[socket.id]);
+
+        // Envia o objeto de jogadores para o novo jogador (server -> client) (emit envia para o socket/jogador referente a sessão)
+        socket.emit('currentPlayers', players);
+
+        // Atualiza todos os outros jogadores do novo jogador (server -> client) (broadcast envia para todos os sockets/jogadores)
+        socket.broadcast.emit('newPlayer', players[socket.id]);
+
         socket.on('disconnect', function() {
             console.log('Jogador desconectou');
+
+            // Remove jogador do servidor
+            removePlayer(self, socket.id);
+
+            // Remove jogador do grupo de jogadores
+            delete players[socket.id];
+
+            // Envia uma mensagem para todos os jogadores removerem esse jogador
+            io.emit('disconnect', socket.id);
+
         });
     });
 }
@@ -38,4 +71,28 @@ function update() {
 
 }
 
+// Game functions
+function addPlayer(self, playerInfo) {
+    const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'player')
+        .setOrigin(0.5, 0.5) // setOrigin(0.5, .05) define o ponto de ancora no meio do sprite
+        .setDisplaySize(53,40); // setDisplaySize() muda a escala do objeto
+    
+    // Configurações de física
+    player.setDrag(100);
+    player.setAngularDrag(100);
+    player.setMaxVelocity(200);
+
+    player.playerId = playerInfo.playerId;
+    self.players.add(player);
+}
+
+function removePlayer(self, playerId) {
+    self.players.getChildren().forEach((player) => {
+        if (playerId === player.playerId) {
+            player.destroy();
+        }
+    });
+}
+
+const game = new Phaser.Game(config);
 window.gameLoaded();
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const path = require('path'); // Referenciando modulo path para trabalhar com diretorios
+const Datauri = require('datauri'); // Referenciando modulo datauri para utilizar metodo createObjectUrl em server
 const jsdom = require('jsdom'); // Referenciando modulo jsdom para 'emular' funções do navegador em servidor
 const express = require('express'); // Referenciando módulo express para funções de app web
 
-const app = express(); // Criando instância em app
+const app = express(); // Instanciando express
+const datauri = new Datauri(); // Instanciando datauri
+const { JSDOM } = jsdom; // Instanciando jsdom
 const server = require('http').Server(app); // Suplantando app para servidor http, para gerenciamento de requests http (GET, POST, etc)
 const io = require('socket.io').listen(server); // Injetando socket.io em servidor para utilizar na comunicação cliente servidor
-const { JSDOM } = jsdom; // Criando instância de Jsdom
 
 app.use(express.static(__dirname + '/public')); // Usando função middleware static de express para renderizar arquivos estáticos
 
@@ -23,6 +25,16 @@ function setupAuthoritativePhaser() {
         // Para que eventos requestAnimationFrame disparem
         pretendToBeVisual: true 
     }).then((dom) => { // Abrindo servidor para clientes após setup do mesmo
+        // Criando metodo createObjectUrl, que ira formatar o blob com datauri no tipo de dado que podemos utilizar
+        dom.window.URL.createObjectURL = (blob) => {
+            if (blob) {
+                return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+            }
+        }
+
+        // Criando metodo revokeObjectUrl
+        dom.window.URL.revokeObjectURL = (objectURL) => {};
+
         dom.window.gameLoaded = () => { // Após Phaser ser carregado e inicializado
             server.listen(8081, function () {
                 console.log(`Listening on ${server.address().port}`);
